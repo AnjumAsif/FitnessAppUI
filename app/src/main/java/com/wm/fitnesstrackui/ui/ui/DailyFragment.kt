15c142a2702d2d13bc4e416db495b6da16871a3c
Asif Anjum <asif.anjum@wm-desk.com>
2019-09-02 18:38:02 +0530

    Code Update: -Update Circular Progress bar

--- app/src/main/java/com/wm/fitnesstrackui/ui/ui/DailyFragment.kt
+++ app/src/main/java/com/wm/fitnesstrackui/ui/ui/DailyFragment.kt
@@ -1,14 +1,21 @@
 package com.wm.fitnesstrackui.ui.ui
 
 import android.content.Context
+import android.graphics.Typeface
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import com.wm.fitnesstrackui.R
+import kotlinx.android.synthetic.main.fragment_daily.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -41,10 +48,22 @@ class DailyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view= inflater.inflate(R.layout.fragment_daily, container, false)
+        val view = inflater.inflate(R.layout.fragment_daily, container, false)
+        val progress: Float = 65f
+        view.circularProgressBar.progress = progress;        // set progress value
+        view.circularProgressBar.maxValue = 100f;            // set progress max value
+        view.circularProgressBar.strokeWidth = 10f;        // set stroke width
+        view.circularProgressBar.backgroundWidth = 28f;        // set progress background width
+        view.circularProgressBar.setProgressColor(
+            ContextCompat.getColor(
+                context as FragmentActivity,
+                R.color.colorPrimary
+            )
+        );    // set progress color
+        view.circularProgressBar.setBackgroundColor("#FFF9916B");    // set progress backgorund color
+        view.circularProgressBar.text = returnTextStyleForSteps();    // set progress text
         return view
     }
-
 
 
     override fun onAttach(context: Context) {
@@ -90,5 +109,67 @@ class DailyFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    /*set color for terms and condition */
+    private fun returnTextStyleForSteps(): String {
+        val stringBuilder = StringBuilder()
+        stringBuilder.append(getString(R.string.steps))
+        stringBuilder.append(" : ")
+        stringBuilder.append(getString(R.string.steps_count))
+        stringBuilder.append("/")
+        stringBuilder.append(getString(R.string.steps_miles))
+
+        val spannableString = SpannableStringBuilder(stringBuilder.toString())
+        spannableString.setSpan(
+            StyleSpan(Typeface.BOLD),
+            stringBuilder.indexOf(getString(R.string.steps)),
+            stringBuilder.indexOf(getString(R.string.steps)).plus(getString(R.string.steps).length),
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannableString.setSpan(
+            StyleSpan(Typeface.ITALIC),
+            stringBuilder.indexOf(getString(R.string.steps_miles)),
+            stringBuilder.indexOf(getString(R.string.steps_miles)).plus(getString(R.string.steps_miles).length),
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+
+
+        /*val stringBuilder = StringBuilder()
+        stringBuilder.append(getString(R.string.terms_condition))
+        stringBuilder.append(" ")
+        stringBuilder.append(getString(R.string.and))
+        stringBuilder.append(" ")
+        stringBuilder.append(getString(R.string.privacy_policy))
+
+        val spannableString = SpannableStringBuilder(stringBuilder.toString())
+        spannableString.setSpan(
+            StyleSpan(Typeface.ITALIC),
+            stringBuilder.indexOf(getString(R.string.terms_condition)),
+            stringBuilder.indexOf(getString(R.string.terms_condition)).plus(getString(R.string.terms_condition).length),
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannableString.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(activity as FragmentActivity, R.color.colorPrimary)),
+            stringBuilder.indexOf(getString(R.string.terms_condition)),
+            stringBuilder.indexOf(getString(R.string.terms_condition)).plus(getString(R.string.terms_condition).length),
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannableString.setSpan(
+            StyleSpan(Typeface.ITALIC),
+            stringBuilder.indexOf(getString(R.string.privacy_policy)),
+            stringBuilder.indexOf(getString(R.string.privacy_policy)).plus(getString(R.string.privacy_policy).length),
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannableString.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(activity as FragmentActivity, R.color.colorPrimary)),
+            stringBuilder.indexOf(getString(R.string.privacy_policy)),
+            stringBuilder.indexOf(getString(R.string.privacy_policy)).plus(getString(R.string.privacy_policy).length),
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )*/
+        return spannableString.toString()
+
     }
 }
